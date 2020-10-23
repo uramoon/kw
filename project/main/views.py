a4532cdcd78ca06ac3779205f48fb84bc2607fe4
variousideas <variousideas@naver.com>
2020-10-24 00:12:56 +0900

    bootstrap 연동

--- project/main/views.py
+++ project/main/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, 'main/index.html')
